b2d73a7906836f4672f742c1566915f507622bf6
kaylacampbell1 <kaylacampbell3003@gmail.com>
2024-07-24 20:23:34 -0400

    created study group page with hard coded data

---
+++ app/app/(tabs)/settings.tsx
@@ -0,0 +1,12 @@
+import { View, SafeAreaView, Text, TextInput } from "react-native";
+import Header from "@/components/Header";
+
+
+export default function Settings() {
+  return (
+    <View style={{ backgroundColor: 'rgba(215,192,174,0.40)', width: '100%', height: '100%' }}>
+      {/* <Header /> */}
+      
+    </View>
+  );
+}
