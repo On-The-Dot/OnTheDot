ffcc0e949f50a5bc93e86dec739c2499bef9c106
kaylacampbell1 <kaylacampbell3003@gmail.com>
2024-08-01 09:56:41 -0400

    fixed routing issue

--- app/app/_layout.tsx
+++ app/app/_layout.tsx
@@ -37,7 +37,7 @@ export default function RootLayout() {
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
         <Stack.Screen name="LoginScreen"  />
-        <Stack.Screen name="SignUp"  />
+        <Stack.Screen name="signUp"  />
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
       </Stack>
