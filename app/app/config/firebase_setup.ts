d47bc73f5835773eaaffa0a2c7745ff90d3b7386
grawang7 <116851264+grawang7@users.noreply.github.com>
2024-07-31 17:53:34 -0700

    merged with main to fix dependency conflicts, may need to install react-native-chart-kit for charts on analytics page

--- app/app/config/firebase_setup.ts
+++ app/app/config/firebase_setup.ts
@@ -7,8 +7,6 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
