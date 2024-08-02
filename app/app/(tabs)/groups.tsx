e5aa51cb834809792813db0e011e1b375ae98d6d
kaylacampbell1 <kaylacampbell3003@gmail.com>
2024-08-01 20:12:06 -0400

    changed study groups to study sessions and added extra fields of data + deleted studyGroupTab component

--- app/app/(tabs)/groups.tsx
+++ app/app/(tabs)/groups.tsx
@@ -1,9 +1,8 @@
-import { View, Text, TextInput, TouchableOpacity, Button } from "react-native";
-import Header from "@/components/Header";
-import StudyGroupTab from "@/components/StudyGroupTab";
+import { View, ScrollView, Text, TextInput, TouchableOpacity, Button, Modal } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
-import { Link } from "expo-router";
+import { ExternalLink } from "@/components/ExternalLink";
 import { db } from "../config/firebase_setup";
+import { getAuth } from "firebase/auth";
 import {
   collection,
   doc,
@@ -17,9 +16,12 @@ import { useState, useEffect } from "react";
 interface StudyGroup {
   id: string;
   description: string;
+  endTime: number;
+  location: string;
   members: Array<string>;
   name: string;
   type: string;
+  startTime: number;
 }
 
 export default function GroupsPage() {
@@ -33,10 +35,16 @@ export default function GroupsPage() {
           (group: QueryDocumentSnapshot<DocumentData>) => ({
             id: group.id,
             ...group.data(),
+            endTime: group.data().endTime["seconds"],
+            startTime: group.data().startTime["seconds"],
           })
         ) as StudyGroup[];
 
-        setStudyGroups(groupsArray);
+        setStudyGroups(
+          groupsArray.sort((a, b) => {
+            return b["startTime"] - a["startTime"];
+          })
+        );
       } catch (error) {
         console.error("Error fetching study groups: ", error);
       }
@@ -44,6 +52,29 @@ export default function GroupsPage() {
 
     fetchGroups();
   }, []);
+
+  const convertTime = (seconds:number) => {
+    const months = [
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "May",
+      "Jun",
+      "Jul",
+      "Aug",
+      "Sep",
+      "Oct",
+      "Nov",
+      "Dec",
+    ];
+    const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    let time = new Date(seconds * 1000);
+    let day = time.getDay()
+    let monthNum = time.getMonth();
+    let date = time.getDate();
+    return `${days[day]} ${months[monthNum]} ${date}  ${time.getHours()}:${time.getMinutes()}`;
+  }
 
   const onPress = () => {};
 
@@ -55,10 +86,10 @@ export default function GroupsPage() {
         height: "100%",
       }}
     >
-      {/* <Header /> */}
       <View
         style={{
           marginTop: 20,
+          marginBottom: 10,
           flexDirection: "row",
         }}
       >
@@ -79,45 +110,81 @@ export default function GroupsPage() {
           <Ionicons name="search" size={25} style={{ margin: 5 }}></Ionicons>
         </TouchableOpacity>
       </View>
-      <View
-        style={{
+      <ScrollView
+        contentContainerStyle={{
           alignItems: "center",
           marginTop: 20,
+          paddingBottom: 50
         }}
       >
         {studyGroups.map((group) => {
           return (
-              <View
-                key={group.id}
+            <View
+              key={group.id}
+              style={{
+                backgroundColor: "rgba(215,192,174,0.47)",
+                width: "87%",
+                // height: 100,
+                margin: 6,
+                paddingTop: 13,
+                paddingLeft: 13,
+                paddingRight: 13,
+                paddingBottom: 8,
+              }}
+            >
+              <Text
                 style={{
-                  backgroundColor: "rgba(215,192,174,0.47)",
-                  width: "87%",
-                  // height: 100,
-                  margin: 6,
-                  paddingTop: 13,
-                  paddingLeft: 13,
-                  paddingRight: 13,
-                  paddingBottom: 8,
+                  fontWeight: 800,
+                  fontSize: 20,
+                  marginBottom: 5,
+                  color: "#8e44ad",
                 }}
               >
-                <Text
-                  style={{
-                    fontWeight: 600,
-                    fontSize: 18,
-                  }}
+                {group.name}
+              </Text>
+              <Text>
+                <Text style={{ fontWeight: 600 }}>Start Time: </Text>
+                {convertTime(group.startTime)}
+              </Text>
+              <Text>
+                <Text style={{ fontWeight: 600 }}>End Time: </Text>
+                {convertTime(group.endTime)}
+              </Text>
+              <Text>
+                <Text style={{ fontWeight: 600 }}>Location: </Text>
+                <ExternalLink
+                  href={group.location}
+                  style={{ color: "rgba(0,102,204,1)" }}
                 >
-                  {group.name}
-                </Text>
-                <Text>{group.description}</Text>
-                <Text style={{ color: "rgba(17,16,16,0.60)", marginTop: 15 }}>
-                  {group.members.length} members
-                </Text>
+                  {group.location}
+                </ExternalLink>
+              </Text>
+              <Text style={{ marginTop: 10 }}>
+                <Text style={{ fontWeight: 600 }}>Note: </Text>
+                {group.description}
+              </Text>
+
+              <View style={{ flexDirection: "row", justifyContent: "space-between", marginLeft: 30, marginRight: 30 }}>
+                {group.members.length === 1 ? (
+                  <Text style={{ color: "rgba(17,16,16,0.60)", marginTop: 15 }}>
+                    1 attendee
+                  </Text>
+                ) : (
+                  <Text style={{ color: "rgba(17,16,16,0.60)", marginTop: 15 }}>
+                    {group.members.length} attendees
+                  </Text>
+                )}
+                <TouchableOpacity style={{ backgroundColor: "purple", width: 60, alignItems: "center", padding: 5, borderRadius: 20, alignSelf: "flex-end", marginTop: 10 }} onPress={() => {alert("Under construction")}}>
+                  <Text style={{ color: "white" }}>Attend</Text>
+                </TouchableOpacity>
               </View>
+              
+            </View>
           );
         })}
-      </View>
+      </ScrollView>
 
-      <View
+      {/* <View
         style={{
           width: 60,
           alignSelf: "flex-end",
@@ -132,7 +199,7 @@ export default function GroupsPage() {
           color="rgba(80,88,225,0.72)"
           accessibilityLabel="button to create a new study group"
         />
-      </View>
+      </View> */}
     </View>
   );
 }
